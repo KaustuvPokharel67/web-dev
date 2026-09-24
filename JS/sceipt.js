1c09e56881c54f61d46a345c45f7e8ebc9d94e83
YOUR_NAME <YOUR_EMAIL@example.com>
2026-09-24 16:23:24 +0545

    Add week 8 JavaScript exercise

---
+++ JS/sceipt.js
@@ -0,0 +1,34 @@
+/*var x = 5;
+var y = 6;
+
+console.log('x', x);
+console.log('y', y);
+
+for (let i = 0; i < 10; i++) {
+    console.log("The value of i is " + i);
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(`2 * ${i} = ${2 * i}`);
+}
+for (let i = 1; i <= 10; i++) {
+    if (i == 3) continue;
+    console.log(`2 * ${i} = ${2 * i}`);
+}
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) break;
+    console.log(`2 * ${i} = ${2 * i}`);
+}
+let i=1;
+while(i<=10){
+    console.log(`2 * ${i} = ${2 * i}`);
+    i++;
+}
+let fruits = ['apple', 'banana', 'cherry'];
+    console.log(fruits[0]);
+    let fruits = ['apple', 'banana', 'cherry'];
+    console.log(fruits[-1]);*/
+    let fruits = ['apple', 'banana', 'cherry'];
+    fruits.push('date');
+    for (let i = 0; i < fruits.length; i++) {
+        console.log(fruits[i]);
+    }
